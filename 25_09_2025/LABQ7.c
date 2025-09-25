3203537bd48d4addbdf7610d6da650142369c3b7
kushgarg016 <kushgarg016@gmail.com>
2025-09-25 12:07:50 +0530

    LABQ7.c

---
+++ 25_09_2025/LABQ7.c
@@ -0,0 +1,26 @@
+//Write a program in C to count the total number of words in a string (with function)./
+
+#include <stdio.h>
+void countWords(char str[]) 
+{
+    int i = 0, words = 0;
+    while(str[i] != '\0') {
+        if(str[i] != ' ' && str[i] != '\n') {           
+            if(str[i+1] == ' ' || str[i+1] == '\n' || str[i+1] == '\0') {
+                words++;                                 
+            }
+        }
+        i++;
+    }
+    printf("Total words: %d\n", words);
+}
+
+int main() 
+{
+    char str[1000];
+    printf("Enter a string = ");
+    fgets(str, sizeof(str), stdin);
+
+    countWords(str);
+    return 0;
+}
