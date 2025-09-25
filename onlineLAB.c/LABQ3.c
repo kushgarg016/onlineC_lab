c6d117060f27716344a05653c2a5b7e7cc2ecf8a
kushgarg016 <kushgarg016@gmail.com>
2025-09-25 11:48:40 +0530

    Create LABQ3.c

---
+++ onlineLAB.c/LABQ3.c
@@ -0,0 +1,28 @@
+//Write a program in C to read a sentence and replace lowercase characters with uppercase and vice versa./
+
+#include <stdio.h>
+#include <string.h>
+    int main()
+{
+    char str[100];
+    int i;
+
+    printf("Enter a string = ");
+    gets(str);
+
+    for(i=0;str[i]!='\0';i++)
+    {
+        if(str[i]>='a' && str[i]<='z')
+        {
+            str[i]=str[i]-32;
+        }
+        else if(str[i]>='A' && str[i]<='Z')
+        {
+            str[i]=str[i]+32;
+        }
+    }
+
+    printf("String after changing case = %s",str);
+
+    return 0;
+}
